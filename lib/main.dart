b2e500f01107e99431fd31b824218f9511d9cdbf
Locus <locuswin@gmail.com>
2025-04-08 10:40:24 -0400

    Adding Envionment config

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:cinemania/config/router/router.dart';
 import 'package:cinemania/config/theme/app_theme.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MainApp());
 }
 
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cinemania/config/constants/environment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,8 +7,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Placeholder(),
-    );
+    return Scaffold(body: Center(child: Text(Environment.movieDBKey)));
   }
 }
